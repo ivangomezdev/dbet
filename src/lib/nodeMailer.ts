be78a0a9728c65d6e3627a7bc89067ebdd760d73
ivan <ivansangomez6@gmail.com>
2025-03-24 09:29:20 -0600

    add responsive home

--- src/lib/nodeMailer.ts
+++ src/lib/nodeMailer.ts
@@ -1,53 +1,21 @@
 import nodemailer from "nodemailer";
+import { Resend } from 'resend';
+
+const resend = new Resend('re_52G6bK3U_5Nqf16iwycdABxV8wF5cBTAw');
+
+
+
 
 export const sendVerificationEmail = async (email: string, code: number) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, // TLS en puerto 587
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: email,
+      subject: 'Tu código para iniciar sesión',
+      html: `<p>Tu código es <strong>${code}</strong>!</p>`
     });
 
-
-
-    await new Promise((resolve, reject) => {
-      // verify connection configuration
-      transporter.verify(function (error, success) {
-          if (error) {
-              console.log(error);
-              reject(error);
-          } else {
-              console.log("Server is ready to take our messages");
-              resolve(success);
-          }
-      });
-  });
-
-    const info = await new Promise((resolve, reject) => {
-      transporter.sendMail(
-        {
-          from: process.env.EMAIL_USER,
-          to: email,
-          subject: "Tu código de verificación",
-          text: `Tu código de verificación es: ${code}`,
-        },
-        (err, info) => {
-          if (err) {
-            console.error("Error enviando email:", err);
-            reject(err);
-          } else {
-            console.log("Email enviado:", info.messageId);
-            resolve(info);
-          }
-        }
-      );
-    });
-
-    return info;
+   
   } catch (error) {
     console.error("Error general al enviar email:", error);
     throw error;
