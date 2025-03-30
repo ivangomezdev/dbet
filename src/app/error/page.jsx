7c3676867054b42b20600c8d376e1345791aa499
ivan <ivansangomez6@gmail.com>
2025-03-29 23:15:19 -0600

    adding failure page

--- src/app/error/page.jsx
+++ src/app/error/page.jsx
@@ -1,16 +1,11 @@
-import { useRouter } from "next/router";
+import React from 'react'
 
-export default function Failed() {
-  const router = useRouter();
-  const { session_id } = router.query;
-
+const page = () => {
   return (
     <div>
-      <h1>Pago Rechazado o Cancelado</h1>
-      <p>El pago no se pudo procesar o fue cancelado. ID de sesión: {session_id}</p>
-      <button onClick={() => router.push("/choose-subscription")}>
-        Volver a elegir plan
-      </button>
+      
     </div>
-  );
+  )
 }
+
+export default page
