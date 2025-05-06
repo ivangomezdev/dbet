8b18a483d32b203d46fddca2fc313fe36cb67463
ivan <ivansangomez6@gmail.com>
2025-05-05 21:40:58 -0600

    changes

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,6 +4,6 @@ import Stripe from 'stripe';
 
 
 
-export const stripe = new Stripe("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");
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '';
