8fcb64fff29460495dcd91169172e1c15689303b
pr-ship-it <developer6969.tech@gmail.com>
2025-05-08 12:06:52 -0500

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,41 @@
 import type { Metadata } from "next";
-import { Geist_Mono } from "next/font/google";
+     import { Geist_Mono } from "next/font/google";
 
-import "./globals.css";
-import { initDB } from '../lib/init-db.js';
-import AllProviders from "./AllProviders";
-import DataFetcher from "./DataFetcher";
+     import "./globals.css";
+     import { initDB } from '../lib/init-db.js';
+     import AllProviders from "./AllProviders";
+     import DataFetcher from "./DataFetcher";
 
-initDB();
+     initDB();
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+     const geistMono = Geist_Mono({
+       variable: "--font-geist-mono",
+       subsets: ["latin"],
+     });
 
-export const metadata: Metadata = {
-  title: "Winbet420 - Gana con matchbetting",
-  description: "Unete",
-  icons: {
-    icon: [
-      { url: 'https://res.cloudinary.com/dllkefj8m/image/upload/v1746029667/WinBet420__4_-removebg-preview_1_pj71ps.png', type: 'image/x-icon' },
-    ],
-  },
-};
+     export const metadata: Metadata = {
+       title: "Winbet420 - Gana con matchbetting",
+       description: "Unete",
+       icons: {
+         icon: [
+           { url: '/favicon.ico', type: 'image/x-icon' },
+         ],
+       },
+     };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={geistMono.variable}>
-        <AllProviders>
-          <DataFetcher />
-          {children}
-        </AllProviders>
-      </body>
-    </html>
-  );
-}
+     export default function RootLayout({
+       children,
+     }: Readonly<{
+       children: React.ReactNode;
+     }>) {
+       return (
+         <html lang="en">
+           <body className={geistMono.variable}>
+             <AllProviders>
+               <DataFetcher />
+               {children}
+             </AllProviders>
+           </body>
+         </html>
+       );
+     }
