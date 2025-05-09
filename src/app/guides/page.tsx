e4449c4a655e7275b8d2046fe4528def924a4c39
pr-ship-it <developer6969.tech@gmail.com>
2025-05-09 11:47:54 -0500

    add changes

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -6,15 +6,13 @@ import React from 'react'
 import "./guides.css"
 import SubscriptionCard from "@/components/SubscriptionCard";
 import { useCookies } from "react-cookie";
-import { useRouter } from "next/navigation";
-import {  useState } from "react";
+
 import { useSession } from "next-auth/react";
 
 const Page = () => {
   const [cookies] = useCookies(["token"]); 
-  const router = useRouter();
-  const [loading, setLoading] = useState(true); // Estado de carga
-  const { data: session , status } = useSession()
+
+  const { data: session } = useSession()
   
 
 
