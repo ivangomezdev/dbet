d42b87974edf4e0665f59863a27be156db2fcac8
ivan <ivansangomez6@gmail.com>
2025-07-11 18:08:11 -0500

    agregando cambios en el front

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from 'stripe';
 
 
 
-export const stripe = new Stripe("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");
+export const stripe = new Stripe("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");
 
 export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '';
